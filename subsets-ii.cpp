fcac7e75322de09a5f25b8f9b2213602f469f14b
wenpincui <wenpincui224@gmail.com>
2014-05-26 18:12:28 +0800

    28/151, subsets-ii

---
+++ subsets-ii.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void dfs(vector<vector<int> >&ans, vector<int> &tmp,
+             vector<int> &S, int size, int pos) {
+        if (pos == size) {
+            if (find(ans.begin(), ans.end(), tmp) == ans.end())
+            ans.push_back(tmp);
+        } else {
+            dfs(ans, tmp, S, size, pos + 1);
+            tmp.push_back(S[pos]);
+            dfs(ans, tmp, S, size, pos + 1);
+            tmp.pop_back();
+        }
+    }
+
+    vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        vector<vector<int> > ans;
+        vector<int> tmp;
+
+        sort(S.begin(), S.end());
+        dfs(ans, tmp, S, S.size(), 0);
+
+        return ans;
+    }
+};
+
+void print(vector<vector<int> > sets)
+{
+    for (int i = 0; i < sets.size(); i++) {
+        vector<int> set = sets[i];
+        for (int j = 0; j < set.size(); j++)
+            printf("%d ", set[j]);
+        printf("\n");
+    }
+}
+int main(int argc, char **argv)
+{
+    vector<int> s;
+    Solution sol;
+
+    s.push_back(1);
+    s.push_back(2);
+    s.push_back(2);
+
+    print(sol.subsets(s));
+}
